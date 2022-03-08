4027f202de8de42f049e77292e3c2bb8a6df8dba
atheboy <amushtaq36@outlook.com>
2022-03-08 21:59:14 +0100

    commit on master

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Button } from 'semantic-ui-react';
 
 export default function App(){
   return (
     <div className="App">
      <h1>Re-vents</h1>
+     <button className='ui icon red button'>
+       <i className='user icon'/> CSS Button
+     </button>
+     <Button />
     </div>
   );
 }
